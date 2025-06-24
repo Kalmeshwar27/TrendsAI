72ad0ff0f9a2404ff8aafec1a47510bbfbafe8ca
Kalmesh12 <kalmeshbirje27@gmail.com>
2025-06-24 17:56:10 +0530

    updated code

--- datajson.py
+++ datajson.py
@@ -3,7 +3,7 @@ import json
 from collections import Counter
 
 # Load Excel
-df = pd.read_excel("data-3.xlsx")
+df = pd.read_excel("data-4.xlsx")
 
 # Clean column names
 df.columns = df.columns.str.strip()
